ca8512507a87bc1ff94716ffcabb0ea502b11fb1
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-10-30 17:55:23 +0300

    Add HomeFragment

--- app/src/main/java/com/example/clientneocafe/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/clientneocafe/view/home/HomeFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.clientneocafe.R
+import com.example.clientneocafe.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dropdown()
+
+    }
+
+    private fun dropdown() {
+        val options = listOf("Вариант 1", "Вариант 2", "Вариант 3")
+
+        val spinner = binding.spinnerOptions
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 
 
