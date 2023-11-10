191fed0072e237f47fa7e8c44acb317adb464a0a
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-11-10 16:00:37 +0300

    Add history layout

--- app/src/main/java/com/example/clientneocafe/view/cart/HistoryFragment.kt
+++ app/src/main/java/com/example/clientneocafe/view/cart/HistoryFragment.kt
@@ -6,15 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.clientneocafe.R
+import com.example.clientneocafe.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
