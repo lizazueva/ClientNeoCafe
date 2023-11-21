d73c31e07a398875fbcff169695917ce178100a7
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-11-21 14:08:14 +0300

    Add date format

--- app/src/main/java/com/example/clientneocafe/view/user/EditProfileFragment.kt
+++ app/src/main/java/com/example/clientneocafe/view/user/EditProfileFragment.kt
@@ -1,18 +1,27 @@
 package com.example.clientneocafe.view.user
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.clientneocafe.R
+import com.example.clientneocafe.databinding.AlertDialogBranchesBinding
+import com.example.clientneocafe.databinding.AlertDialogEditProfileBinding
 import com.example.clientneocafe.databinding.FragmentEditProfileBinding
 import com.example.clientneocafe.model.auth.User
+import com.example.clientneocafe.utils.DateMask
 import com.example.clientneocafe.utils.Resource
 import com.example.clientneocafe.viewModel.UserViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class EditProfileFragment : Fragment() {
 
@@ -37,11 +46,34 @@ class EditProfileFragment : Fragment() {
 
     private fun setUpListeners() {
         binding.imageBack.setOnClickListener {
-            findNavController().navigate(R.id.action_editProfileFragment_to_userFragment)
+            showAlertDialog()
         }
         binding.btnSave.setOnClickListener {
-            updateDataProfile()
-            observeEditDataProfile()
+            val date = binding.textInputDate.text.toString()
+            if (validateDate(date)){
+                updateDataProfile()
+                observeEditDataProfile()
+            }
+        }
+    }
+
+    private fun showAlertDialog() {
+        val dialogBinding = AlertDialogEditProfileBinding.inflate(layoutInflater)
+        val dialog = Dialog(requireContext())
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        dialogBinding.buttonNo.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialogBinding.buttonYes.setOnClickListener {
+            dialog.dismiss()
+            findNavController().navigate(R.id.action_editProfileFragment_to_userFragment)
         }
     }
 
@@ -65,19 +97,53 @@ class EditProfileFragment : Fragment() {
     private fun setDataEditProfile() {
         val profile = arguments?.getParcelable<User>("user")
 
+        val editTextDate = binding.textInputDate
+        val dateMask = DateMask(editTextDate)
+        editTextDate.addTextChangedListener(dateMask)
+
         profile?.let {
             binding.textInputName.setText(it.first_name)
             binding.textInputPhone.setText(it.phone_number)
-            binding.textInputDate.setText(it.birth_date)
+            val date = it.birth_date.toString()
+            val formattedDate = convertDateFormatGet(date, "yyyy-MM-dd", "MM.dd.yyyy")
+            binding.textInputDate.setText(formattedDate)
 
         }
     }
 
-    fun updateDataProfile(){
+    private fun updateDataProfile(){
         val name = binding.textInputName.text.toString().trim()
         val phone = binding.textInputPhone.text.toString().trim()
         val date = binding.textInputDate.text.toString().trim()
+        val formattedDate = convertDateFormatSent(date, "MM.dd.yyyy", "yyyy-MM-dd")
 
-        userViewModel.updateProfile(phone, name, date)
+        userViewModel.updateProfile(phone, name, formattedDate)
+    }
+
+    private fun convertDateFormatSent(dateInput: String, inputFormat: String, outputFormat: String): String {
+        val inputFormatter = SimpleDateFormat(inputFormat, Locale.getDefault())
+        val outputFormatter = SimpleDateFormat(outputFormat, Locale.getDefault())
+        val parsedDate = inputFormatter.parse(dateInput)
+        return outputFormatter.format(parsedDate)
+    }
+
+    private fun convertDateFormatGet(dateInput: String, inputFormat: String, outputFormat: String): String {
+        val inputFormatter = SimpleDateFormat(inputFormat, Locale.getDefault())
+        val outputFormatter = SimpleDateFormat(outputFormat, Locale.getDefault())
+        val parsedDate = inputFormatter.parse(dateInput)
+        return outputFormatter.format(parsedDate)
+    }
+
+    private fun validateDate(dateInput: String):Boolean {
+        val isDateMatches = dateInput.matches(Regex("\\d{2}\\.\\d{2}\\.\\d{4}"))
+
+        if (!isDateMatches) {
+            binding.textInputDate.error = "Неверный формат даты"
+            binding.textInputDate.setTextColor(Color.RED)
+            return false
+        } else {
+            binding.textInputDate.error = null
+            return true
+        }
     }
 }
--- app/src/main/java/com/example/clientneocafe/view/user/UserFragment.kt
+++ app/src/main/java/com/example/clientneocafe/view/user/UserFragment.kt
@@ -1,11 +1,17 @@
 package com.example.clientneocafe.view.user
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.navigation.fragment.findNavController
+import com.example.clientneocafe.databinding.AlertDialogBranchesBinding
+import com.example.clientneocafe.databinding.AlertDialogInfoBonusesBinding
 import com.example.clientneocafe.databinding.FragmentUserBinding
 import com.example.clientneocafe.model.auth.User
 import com.example.clientneocafe.utils.Resource
@@ -40,8 +46,6 @@ class UserFragment : Fragment() {
             when (user) {
                 is Resource.Success -> {
                     binding.textUserName.text = user.data?.first_name
-//                    profile = user.data.takeIf { it != null }
-
                     user.data?.let { profile = it }
                 }
                 is Resource.Loading ->{
@@ -58,6 +62,25 @@ class UserFragment : Fragment() {
         binding.imageEditProfile.setOnClickListener {
             val action = UserFragmentDirections.actionUserFragmentToEditProfileFragment(profile!!)
             findNavController().navigate(action)
+        }
+        binding.cardForBonuses.setOnClickListener {
+            showDialogBonuses()
+
+        }
+    }
+
+    private fun showDialogBonuses() {
+        val dialogBinding = AlertDialogInfoBonusesBinding.inflate(layoutInflater)
+        val dialog = Dialog(requireContext())
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        dialogBinding.buttonOk.setOnClickListener {
+            dialog.dismiss()
         }
     }
 }
